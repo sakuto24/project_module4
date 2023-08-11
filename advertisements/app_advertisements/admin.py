c9d168136cf4aa93dc009231957b9890b3d7b70b
azat <azat.gabdrakh2408@gmail.com>
2023-08-11 17:20:08 +0300

    '8ЗанятиеДз'

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from .models import Advertisement  # импортирую свою модель
+# класс объекта
+from django.db.models.query import QuerySet
 
-# Register your models here.
+
+# py manage.py createuser - создание аккаунта супер пользователя
+# http://127.0.0.1:8000/admin
+
+
+# класс для кастомизации модели в админке
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'auction', 'created_date', 'updated_date']
+    list_filter = ['auction', 'created_at', 'price']  # столбцы по которым будет фильтрация
+    actions = ['make_auction_as_false', 'make_auction_as_true']  # методы для выбранных записей
+    fieldsets = (
+        ('Общие', {  # блок 1
+            "fields": (
+                'title', 'description'  # поля блока
+            ),
+        }),
+        ('Финансы', {  # блок 2
+            "fields": (
+                'price', 'auction'  # поля блока
+            ),
+            'classes': ['collapse'] # скрыть/показать блок
+        })
+    )
+
+
+    @admin.action(description='Добавить возможность торга')
+    def make_auction_as_true(self, request, queryset: QuerySet):
+        queryset.update(auction=True)
+
+    @admin.action(description='Убрать возможность торга')
+    def make_auction_as_false(self, request, queryset: QuerySet):
+        queryset.update(auction=False)
+
+
+# подключение модели в админку и кастомной модели
+admin.site.register(Advertisement, AdvertisementAdmin)
+
+
+"""def dec(func):
+    def wrapper():
+        print()
+        func()
+        print()
+
+    return
+
+
+def add_list(some_list: list):
+    some_list
+"""
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+# модули для настройки методов админки
+from django.contrib import admin
+from django.utils import timezone # для времени
+from django.utils.html import format_html # для создания строки html
+import datetime
+
 class Advertisement(models.Model):
     title = models.CharField('заголовок', max_length=128)
     description = models.TextField('описание')
@@ -9,10 +15,31 @@ class Advertisement(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+
+
     class Meta:
         db_table = 'advertisements'
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
 
+    @admin.display(description='дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():  # проверяю что запись была создана сегодня
+            created_time = self.created_at.time().strftime('%H:%M:%S')  # 19:30:15
+            return format_html(
+                "<span style='color:green; font-weight: bold'>Сегодня в {}</span>",
+                created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y at %H:%M:%S')  # 04.08.2023 at 19:30:15
+    # метод если запись была создана сегодня то мы отобразим ее зеленым цветом, если не сегодня , то серым
 
+    @admin.display(description='дата обновления')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():  # проверяю что запись была создана сегодня
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')  # 19:30:15
+            return format_html(
+                "<span style='color:pink; font-weight: bold'>Сегодня в {}</span>",
+                updated_time
+            )
+        return self.updated_at.strftime('%d.%m.%Y at %H:%M:%S')  # 04.08.2023 at 19:30:15
